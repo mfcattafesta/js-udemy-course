75830db592f31f9219c3a53fad387f949c73a67a
mfcattafesta <mfcattafesta@gmail.com>
2019-04-09 20:44:46 -0300

    add comment to challenge 2

--- 1-js-basics/coding_challenge_2.js
+++ 1-js-basics/coding_challenge_2.js
@@ -20,21 +20,23 @@ var scoreMike = (129 + 94 + 123) / 3;
 var scoreMary = (97 + 134 + 105) / 3;
 console.log(scoreJohn, scoreMike, scoreMary);
 
+
+// Only 2 players
+if (scoreJohn > scoreMike) {
+    console.log('John\'s team wins with ' + scoreJohn + ' points');
+} else if (scoreMike > scoreJohn) {
+    console.log('Mike\'s team wins with ' + scoreMike + ' points');
+} else {
+    console.log('There is a draw');
+}
+
+// 3 players
 if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
     console.log('John\'s team wins with ' + scoreJohn + ' points');
 } else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
     console.log('Mike\'s team wins with ' + scoreMike + ' points');
 } else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
     console.log('Mary\'s team wins with ' + scoreMary + ' points');
-} else {
-    console.log('There is a draw');
-}
-
-
-if (scoreJohn > scoreMike) {
-    console.log('John\'s team wins with ' + scoreJohn + ' points');
-} else if (scoreMike > scoreJohn) {
-    console.log('Mike\'s team wins with ' + scoreMike + ' points');
 } else {
     console.log('There is a draw');
 }
